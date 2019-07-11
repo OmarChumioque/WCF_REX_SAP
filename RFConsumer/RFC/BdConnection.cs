80b033b3b22e95c028ee28ca1b5811127331361e
omarchdev <omarchdev@gmail.com>
2019-07-11 09:21:53 -0500

    bdconnect

---
+++ RFConsumer/RFC/BdConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFC
+{
+    class BdConnection
+    {
+        SqlConnection conn;
+
+        public BdConnection() {
+
+            conn = new SqlConnection("Data Source=185.144.157.97;" +
+                "Initial Catalog=Rex;" +
+                "User Id=omarch1409;Password=1409Chumioque;" +
+                "connect timeout=2000;");
+
+        }
+
+        public void AgregarMovimientos() {
+
+        } 
+
+    }
+}
