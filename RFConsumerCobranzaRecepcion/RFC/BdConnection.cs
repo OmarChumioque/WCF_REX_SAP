b5809f67a837e3493cbad0a828f2435da4b75728
omarchdev <omarchdev@gmail.com>
2019-07-13 19:38:33 -0500

    Recepcion datos cobranza SAP A REX

---
+++ RFConsumerCobranzaRecepcion/RFC/BdConnection.cs
@@ -0,0 +1,74 @@
+﻿using RFC.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFC
+{
+    public class BdConnection
+    {
+        SqlConnection conn;
+
+        public BdConnection() {
+
+            conn = new SqlConnection("Data Source=172.31.236.221;" +
+               "Initial Catalog=rex;" +
+               "User Id=rexdb;Password=rexdb2019;" +
+               "connect timeout=2000;");
+        }
+
+       
+        public void AgregarDatosCobranza(DataTable dt)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("delete DocSAPCobranzaRec", conn);
+                cmd.ExecuteNonQuery();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    SqlBulkCopy bulk = new SqlBulkCopy(conn);
+                    bulk.DestinationTableName = "DocSAPCobranzaRec";
+                    bulk.ColumnMappings.Add("KUNNR", "KUNNR");
+                    bulk.ColumnMappings.Add("BURKS", "BURKS");
+                    bulk.ColumnMappings.Add("BELNR_D", "BELNR_D");
+                    bulk.ColumnMappings.Add("GJAHR", "GJAHR");
+                    bulk.ColumnMappings.Add("BUZEI", "BUZEI");
+                    bulk.ColumnMappings.Add("BUDAT", "BUDAT");
+                    bulk.ColumnMappings.Add("DZUONR", "DZUONR");
+                    bulk.ColumnMappings.Add("XBLNR1", "XBLNR1");
+                    bulk.ColumnMappings.Add("SHKZG", "SHKZG");
+                    bulk.ColumnMappings.Add("WAERS", "WAERS");
+                    bulk.ColumnMappings.Add("DMBTR", "DMBTR");
+                    bulk.ColumnMappings.Add("WRBTR", "WRBTR");
+                    bulk.ColumnMappings.Add("DZFBDT", "DZFBDT");
+                    bulk.ColumnMappings.Add("DATS", "DATS");
+                    bulk.ColumnMappings.Add("UZEIT", "UZEIT");
+
+                    bulk.WriteToServer(dt);
+                   /* SqlCommand stored = new SqlCommand("pIntmovalmacenstock", conn);
+                    stored.CommandType = CommandType.StoredProcedure;
+                    stored.ExecuteNonQuery();*/
+                }
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+            }
+            finally {
+                if (conn.State == ConnectionState.Open) {
+                    conn.Close();
+                }
+            }
+
+
+
+
+        } 
+
+    }
+}
---
+++ RFConsumerCobranzaRecepcion/RFC/RfcInvoke.cs
@@ -0,0 +1,127 @@
+﻿using RFC.Model;
+using SAP.Middleware.Connector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFC
+{
+    public class RfcInvoke
+    {
+
+        private string iM_FEC_INI;
+        private string iM_FEC_FIN;
+        private string I_BUDAT;
+
+        public RfcInvoke(string I_BUDAT)
+        {
+            this.I_BUDAT = I_BUDAT;
+        }
+
+        public RfcInvoke() {
+        }
+        public RfcInvoke(string FEC_INI, string FEC_FIN)
+        {
+            this.iM_FEC_INI = FEC_INI;
+            this.iM_FEC_FIN = FEC_FIN;
+        }
+
+        public DataTable ObtenerDatosCobranza()
+        {
+
+          
+            RfcConfigParameters rfc = new RfcConfigParameters();
+            rfc.Add(RfcConfigParameters.Name, "Desarrollo");
+            rfc.Add(RfcConfigParameters.AppServerHost, "10.16.1.30");
+            rfc.Add(RfcConfigParameters.Client, "600");
+            rfc.Add(RfcConfigParameters.User, "REXSAP2");
+            rfc.Add(RfcConfigParameters.Password, "Rexsap01");
+            rfc.Add(RfcConfigParameters.SystemNumber, "00");
+            rfc.Add(RfcConfigParameters.Language, "ES");
+            rfc.Add(RfcConfigParameters.PoolSize, "5");
+            rfc.Add(RfcConfigParameters.MaxPoolSize, "100");
+            rfc.Add(RfcConfigParameters.IdleTimeout, "900");
+            RfcDestination rfcDest = null;
+            RfcRepository rfcRep = null;
+
+            try
+            {
+                rfcDest = RfcDestinationManager.GetDestination(rfc);
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+            }
+
+            IRfcFunction function = rfcDest.Repository.CreateFunction("ZSD_REXSAP_009");
+         
+            try
+            {
+                function.Invoke(rfcDest);
+                IRfcTable doc = function.GetTable("M_PEDIDOS");
+                DataTable table = IRfcTable_To_DataTable(doc, "MOVALMACEN");
+                return table;
+            }
+            catch (RfcBaseException e)
+            {
+                return null;    
+            }
+
+
+    
+       
+          
+        }
+        private  DataTable IRfcTable_To_DataTable(IRfcTable doc, string tableName) {
+            DataTable table = new DataTable(tableName);
+
+            for (int i = 0; i < doc.ElementCount; i++)
+            {
+                RfcElementMetadata metadata = doc.GetElementMetadata(i);
+                if (metadata.DataType.ToString().Equals("CHAR"))
+                {
+                    table.Columns.Add(metadata.Name, System.Type.GetType("System.String"));
+                }
+                if (metadata.DataType.ToString().Equals("BCD"))
+                {
+                    table.Columns.Add(metadata.Name, System.Type.GetType("System.Decimal"));
+                }
+                if (metadata.DataType.ToString().Equals("DATE"))
+                {
+                    table.Columns.Add(metadata.Name, System.Type.GetType("System.String"));
+                }
+            }
+
+            foreach (IRfcStructure row in doc)
+            {
+                DataRow dr = table.NewRow();
+                for (int i = 0; i < doc.ElementCount; i++)
+                {
+                    RfcElementMetadata metadata = doc.GetElementMetadata(i);
+                    if (metadata.DataType.ToString().Equals("CHAR"))
+                    {
+                 
+                          dr[metadata.Name] = row.GetString(metadata.Name);
+                                   
+                    }
+                    if (metadata.DataType.ToString().Equals("BCD"))
+                    {
+                        dr[metadata.Name] = row.GetDecimal(metadata.Name);
+                    }
+                    if (metadata.DataType.ToString().Equals("DATE"))
+                    {
+                        dr[metadata.Name] = row.GetString(metadata.Name);
+                    }
+                }
+                table.Rows.Add(dr);
+            }
+            return table;
+        }
+    }
+
+
+}
